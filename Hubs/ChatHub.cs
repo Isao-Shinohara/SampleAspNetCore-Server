87d6b43d146edaae7d9aabb8c7a08405610c640a
Isao-Shinohara <shinohara@double-rabbits.net>
2017-06-06 08:51:37 +0900

    add ChatHub

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat
+{
+	public class ChatHub : Hub
+	{
+		public void Send(string message)
+		{
+			Console.WriteLine(String.Format("[Called '{0}'] {1}", MethodBase.GetCurrentMethod().Name, message));
+			Clients.All.Recieve(message);
+		}
+	}
+}
