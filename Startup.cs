54e7d257c769ded59e7322ee06c7ddb451060405
Isao-Shinohara <shinohara@double-rabbits.net>
2017-06-19 23:17:45 +0900

    services.AddCors

--- Startup.cs
+++ Startup.cs
@@ -50,6 +50,7 @@ namespace SampleAspNetCore_Server
 			});
 
 			services.AddSignalR();
+			services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
