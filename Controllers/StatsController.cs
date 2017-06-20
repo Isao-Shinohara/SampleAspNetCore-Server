c0c3bfb442aaafa5dc39afcf77917e8675461e0e
Isao-Shinohara <shinohara@double-rabbits.net>
2017-06-20 22:22:43 +0900

    add API of SignalR Connection Num

---
+++ Controllers/StatsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SignalRChat;
+
+namespace SampleAspNetCore_Server.Controllers
+{
+	[FormatFilter]
+	[Route("api/[controller]")]
+    public class StatsController : Controller
+    {
+		// GET api/values
+		[HttpGet]
+		[Route("{format?}")]
+		public IActionResult Get()
+		{
+			int connectionNum = ChatHub.ConnectedIds.Count();
+			Console.WriteLine(String.Format("Connection Num: {0}", connectionNum));
+
+			return Ok(connectionNum);
+		}
+    }
+}
---
+++ Models/Stats.cs
@@ -0,0 +1,8 @@
+﻿using MessagePack;
+
+[MessagePackObject]
+public class Stats
+{
+	[Key(0)]
+	public int ConnectionNum { get; set; }
+}
